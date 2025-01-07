743a5b53232ac4b60967f20c50031fefc7582421
vivek pathak <vivekkpaathak@gmail.com>
2025-01-07 22:11:15 +0530

    modified div function

--- calculator.java
+++ calculator.java
@@ -1,22 +1,38 @@
 public class Calculator{
 
-public static int div(int a, int b)
+  public static int add(int a, int b)
+  {
+    return a+b;
+  }
+
+  public static int div(int a, int b)
 {
   return a/b;
 }
-
-
-
-
-public static void main(String [] args)
-{
-          System.out.println("welcome to calculator !");
-          System.out.println("divided two number 5 and 7" + " : "+div(5,7));
-        
-
-
-
-
-   
-}
-}
+  
+  public static int sub(int a, int b)
+  {
+    return a-b;
+  }
+  public static int mul(int a, int b)
+  {
+    return a*b;
+  }
+  
+  
+  
+  public static void main(String [] args)
+  {
+            System.out.println("welcome to calculator !");
+            System.out.println("added two number 5 and 7" + " : "+add(5,7));
+            System.out.println("subtract 5 from 7" + " : "+sub(7,5));
+             System.out.println("multiplied two number 5 and 7" + " : "+mul(5,7));
+             System.out.println("divided two number 5 and 7" + " : "+div(5,7));
+  
+  
+  
+  
+  
+     
+  }
+  }
